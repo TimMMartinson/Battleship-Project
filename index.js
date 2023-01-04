9fc7a12e73c3ad52cd56af10c58570ca140be7e8
timmmartinson <timmmartinson@gmail.com>
2023-01-04 13:42:49 -0700

    fixed player ship placement, ai ship placement still buggy

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         const cell = document.createElement("div")
         cell.classList.add("cell")
-        cell.addEventListener("click", handlePlayerAttack)
         playerGrid.appendChild(cell)
 
         const aiCell = document.createElement("div")
         aiCell.classList.add("cell")
+        aiCell.addEventListener("click", handlePlayerAttack)
         aiGrid.appendChild(aiCell)
     }
 }
 
-// Placing player's ships
+// Placing AI ships and initializing Player Ship placement
 
 startButton.addEventListener("click", () => {
     playerGrid.querySelectorAll(".cell").forEach(cell => {
@@ -56,52 +56,76 @@ function handlePlayerShipPlacement(evt) {
             }
         }
     }
+    if (playerShips.length === 0) {
+        return
+    }
 }
 
-function handleAIShipPlacement() {
-    for (const shipLength of aiShips) {
-      // Pick a random cell to start the ship
-      const startRow = Math.floor(Math.random() * 10)
-      const startCol = Math.floor(Math.random() * 10)
-      const startCell = aiGrid.children[startRow].children[startCol]
+function handleAIShipPlacement(ships) {
+    if(!aiGrid){
+        return
+    }
+
+    // sort ship lengths in descending order
+    const sortedShipLengths = [...ships].sort((a, b) => b - a)
   
-      // Pick a random orientation for the ship (horizontal or vertical)
-      const orientation = Math.random() < 0.5 ? "horizontal" : "vertical"
+    // place ships on AI grid
+    for (const shipLength of sortedShipLengths) {
+      let placed = false
+      let attempts = 0
+      // This prevents the while loop from going infinite
+      while (!placed && attempts < 1000) {
+        // Pick a random cell to start the ship
+        const startRow = Math.floor(Math.random() * 10)
+        const startCol = Math.floor(Math.random() * 10)
+        const startCell = aiGrid.children[startRow].children[startCol]
   
-      let isValidPlacement = true
-      for (let i = 0; i < shipLength; i++) {
-        const nextCell = getNextCell(startCell, i, orientation)
-        if (!nextCell || nextCell.classList.contains("ship")) {
-          isValidPlacement = false
+        // Pick a random orientation for the ship (horizontal or vertical)
+        const orientation = Math.random() < 0.5 ? "horizontal" : "vertical"
+  
+        let isValidPlacement = true
+        for (let i = 0; i < shipLength; i++) {
+          const nextCell = getNextCell(startCell, i, orientation)
+          if (!nextCell || nextCell.classList.contains("ship")) {
+            isValidPlacement = false
+            break
+          }
+        }
+  
+        if (isValidPlacement) {
+          // Place the ship
+          startCell.classList.add("ship")
+          startCell.dataset.length = shipLength
+          for (let i = 1; i < shipLength; i++) {
+            const nextCell = getNextCell(startCell, i, orientation)
+            nextCell.classList.add("ship")
+          }
+          placed = true
+        } else {
+          // Cannot place the ship, try again with next ship
+          placed = false
+          ships.push(shipLength)
+        }
+  
+        // Break out of the loop if we've reached 1000 attempts
+        attempts++
+        if (attempts === 1000) {
           break
         }
       }
-  
-      if (isValidPlacement) {
-        // Place the ship
-        startCell.classList.add("ship")
-        startCell.dataset.length = shipLength
-        for (let i = 1; i < shipLength; i++) {
-          const nextCell = getNextCell(startCell, i, orientation)
-          nextCell.classList.add("ship")
-        }
-      } else {
-        // Cannot place the ship, try again
-        aiShips.push(shipLength)
-      }
     }
   }
-
+  
   
   function getNextCell(startCell, i, orientation) {
     if (!startCell) return null
     if (orientation === "horizontal") {
       // Find the next cell in the same row
-      return startCell.nextSibling
+      return startCell.nextElementSibling
     } else if (orientation === "vertical") {
       // Find the next cell in the same column
-      if (startCell.parentElement && startCell.parentElement.nextSibling) {
-        return startCell.parentElement.nextSibling.children[startCell.cellIndex]
+      if (startCell.parentElement && startCell.parentElement.nextElementSibling) {
+        return startCell.parentElement.nextElementSibling.children[startCell.cellIndex]
       } else {
         return null
       }
