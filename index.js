2f2d6cb54414c485afb8732bb55d37a7e6b369a5
timmmartinson <timmmartinson@gmail.com>
2023-01-04 07:28:33 -0700

    added js to make grids for player and ai

---
+++ index.js
@@ -0,0 +1,18 @@
+// declaring board variables
+const playerGrid = document.getElementById("playerGrid")
+const aiGrid = document.getElementById("aiGrid")
+const startButton = document.getElementById("startButton")
+
+// creating grids
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        const cell = document.createElement("div")
+        cell.classList.add("cell")
+        playerGrid.appendChild(cell)
+
+        const aiCell = docmuent.createElement("div")
+        aiCell.classList.add("cell")
+        aiGrid.appendChild(aiCell)
+    }
+}
